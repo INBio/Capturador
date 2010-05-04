14acf973aed6135ae1ae5f154b6496db8302ec20
gsulca <gsulca@3aa2e761-397d-4522-80f4-092673de1051>
2010-05-04 22:38:00 +0000

    Actualización para Indicadores
    git-svn-id: svn://pulsatrix.inbio.ac.cr/atta2/trunk@1282 3aa2e761-397d-4522-80f4-092673de1051

--- ara-web/src/java/org/inbio/ara/indicator/TreeController.java
+++ ara-web/src/java/org/inbio/ara/indicator/TreeController.java
@@ -38,35 +38,15 @@ public class TreeController {
         scriptString += "tree.setDynamicLoad(loadNodeData, currentIconMode);\n";
                 //Getting a reference to the root element
         scriptString += "rootNode = tree.getRoot();\n";
-        scriptString += "var rootInformation = getNode(idRoot);\n";
-        //scriptString += "alert('getHiddenNodeId() = '+getHiddenNodeId());\n";
+        scriptString += "var rootInformation = getNode(idRoot);\n";        
         scriptString += "if(nodeId != 0)\n";
         scriptString += "{\n";
-        scriptString += "expand = true;\n";
-        //scriptString += "alert('se debe expandir');\n";
+        scriptString += "expand = true;\n";        
         scriptString += "}\n";
                 //Add the root element
         scriptString += "var tempNode = new YAHOO.widget.TextNode(rootInformation[1], rootNode, expand);\n";
-        scriptString += "tempNode.data = idRoot;\n";
-      //  scriptString += "tree.expandAll();\n";
-        //scriptString += "tree.render();\n";
-                //Render the tree
-        
-                //Suscribing the event to get the selected node
-        /*scriptString += "tree.subscribe('clickEvent',function(oArgs) {\n";
-        scriptString += "selectedNodeId = oArgs.node.data;\n";
-        scriptString += "selectedNodeName = oArgs.node.label;\n";        
-        scriptString += "setHiddenNodeId(selectedNodeId);\n";
-        scriptString += "setHiddenPathNode(getPathNode(oArgs.node));\n";
-        scriptString += "if(oArgs.node.isLeaf){\n";
-        scriptString += "isLeaf = 'true';\n";
-        scriptString += "}\n";
-        scriptString += "else{\n";
-        scriptString += "isLeaf = 'false';\n";
-        scriptString += "}\n";
-        scriptString += "});\n";*/
-        scriptString += "tree.subscribe('clickEvent',onClickEvent);\n";
-        //scriptString += "tree.collapseAll();\n";
+        scriptString += "tempNode.data = idRoot;\n";   
+        scriptString += "tree.subscribe('clickEvent',onClickEvent);\n";        
         scriptString += "tree.render();\n";
         scriptString += "}\n\n";
 
