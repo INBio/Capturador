9d43dbb1f14ae966e46f769ffe9012af769d6413
gsulca <gsulca@3aa2e761-397d-4522-80f4-092673de1051>
2010-06-17 17:35:54 +0000

    Agregar los esao para la relacion indicator-dublin_core
    git-svn-id: svn://pulsatrix.inbio.ac.cr/atta2/trunk@1378 3aa2e761-397d-4522-80f4-092673de1051

---
+++ ara-ejb/src/java/org/inbio/ara/eao/indicator/IndicatorDublinCoreEAOLocal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.inbio.ara.eao.indicator;
+
+import javax.ejb.Local;
+import org.inbio.ara.eao.BaseLocalEAO;
+import org.inbio.ara.persistence.indicator.IndicatorDublinCore;
+
+/**
+ *
+ * @author gsulca
+ */
+@Local
+public interface IndicatorDublinCoreEAOLocal extends BaseLocalEAO<IndicatorDublinCore,Long>{
+    
+}
---
+++ ara-ejb/src/java/org/inbio/ara/eao/indicator/impl/IndicatorDublinCoreEAOImpl.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.inbio.ara.eao.indicator.impl;
+
+
+import org.inbio.ara.eao.indicator.*;
+import javax.ejb.Stateless;
+import org.inbio.ara.eao.BaseEAOImpl;
+import org.inbio.ara.persistence.indicator.IndicatorDublinCore;
+
+/**
+ *
+ * @author gsulca
+ */
+@Stateless
+public class IndicatorDublinCoreEAOImpl extends BaseEAOImpl<IndicatorDublinCore, Long> implements IndicatorDublinCoreEAOLocal {
+
+
+ 
+}
