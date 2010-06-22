dc81e5406a517e6503e2a8e8f76ddb5a3012a2d5
gsulca <gsulca@3aa2e761-397d-4522-80f4-092673de1051>
2010-06-22 16:37:35 +0000

    git-svn-id: svn://pulsatrix.inbio.ac.cr/atta2/trunk@1382 3aa2e761-397d-4522-80f4-092673de1051

---
+++ ara-ejb/src/java/org/inbio/ara/facade/indicator/IndicatorFacadeRemote.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.inbio.ara.facade.indicator;
+
+import java.util.List;
+import javax.ejb.Remote;
+import org.inbio.ara.dto.indicator.IndicatorDTO;
+
+/**
+ *
+ * @author gsulca
+ */
+@Remote
+public interface IndicatorFacadeRemote {
+
+    public List<IndicatorDTO> getChildrenByIndicatorId(Long indicatorId);
+
+    public IndicatorDTO getIndicatorByIndicatorId(Long indicatorId);
+
+    public IndicatorDTO saveNewIndicator(IndicatorDTO iDTO);
+
+    public IndicatorDTO updateIndicator(IndicatorDTO iDTO);
+
+    public Long countChildrenByIndicatorId(Long indicatorId);
+
+    public void deleteIndicator(Long IndicatorId);
+
+    public void saveIndicatorDublinCores(Long indicatorId, List<String> dublinCoreIds, String userName);
+
+
+    
+}
