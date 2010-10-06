170e4e7708540d2ba55e28c1dafdef7b6508b399
gsulca <gsulca@3aa2e761-397d-4522-80f4-092673de1051>
2010-10-06 18:58:06 +0000

    Actualizacion para TaxonIndicatorCountry y TaxonIndicatorDublinCore
    git-svn-id: svn://pulsatrix.inbio.ac.cr/atta2/trunk@1481 3aa2e761-397d-4522-80f4-092673de1051

---
+++ ara-ejb/src/java/org/inbio/ara/eao/taxonomy/TaxonIndicatorDublinCoreEAOLocal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.inbio.ara.eao.taxonomy;
+
+import java.util.List;
+import javax.ejb.Local;
+import org.inbio.ara.eao.BaseLocalEAO;
+import org.inbio.ara.persistence.taxonomy.TaxonIndicatorDublinCore;
+
+/**
+ *
+ * @author gsulca
+ */
+@Local
+public interface TaxonIndicatorDublinCoreEAOLocal extends BaseLocalEAO<TaxonIndicatorDublinCore,Long> {
+
+    public List<Long> findDublinCoreByTaxonIndicatorIds(Long taxonId, Long indicatorId);
+
+    public void deleteTaxonIndicatorDublinCoreById(Long taxonId, Long indicatorId, Long dublinCoreId);
+
+    public void deleteTaxonIndicatorDublinCoreByTaxonId(Long taxonId);
+    
+}
