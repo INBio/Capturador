37ea48f8c32653073c9fe91e907468a84b9d65ce
gsulca <gsulca@3aa2e761-397d-4522-80f4-092673de1051>
2011-03-31 14:46:14 +0000

    Actualizacion del SelectionListEntity, los id que tenia no coincidian con los de la bd
    git-svn-id: svn://pulsatrix.inbio.ac.cr/atta2/trunk@1606 3aa2e761-397d-4522-80f4-092673de1051

--- atta-ejb/src/java/org/inbio/ara/dto/inventory/SelectionListEntity.java
+++ atta-ejb/src/java/org/inbio/ara/dto/inventory/SelectionListEntity.java
@@ -50,60 +50,60 @@ import org.inbio.ara.persistence.transaction.TransactionType;
  */
 public enum SelectionListEntity {
     //EXIFMetadataExtractorDAOImpl.class.getName()
-    GATHERING_METHOD_OBSERVATION(new Long(1), new GatheringObservationMethod(), true,"sle_gathering_method_observation", GatheringObservationMethod.class.getName()),
-    SPECIMEN_CATEGORY(new Long(2), new SpecimenCategory(), false,"sle_specimen_category", SpecimenCategory.class.getName()),
-    SPECIMEN_TYPE(new Long(3), new SpecimenType(), true,"sle_specimen_type",SpecimenType.class.getName()),
-    ORIGIN(new Long(4), new Origin(), true,"sle_origin",Origin.class.getName()),
-    PRESERVATION_MEDIUM(new Long(5), new PreservationMedium(), true,"sle_preservation_medium",PreservationMedium.class.getName()),
-    STORAGE_TYPE(new Long(6), new StorageType(), true,"sle_storage_type",StorageType.class.getName()),
-    LIFE_STAGE(new Long(7), new LifeStage(), true,"sle_life_stage",LifeStage.class.getName()),
-    SEX(new Long(8), new Sex(), true,"sle_sex",Sex.class.getName()),
-    SUBSTRATE(new Long(9), new Substrate(), true,"sle_substrate",Substrate.class.getName()),
+    GATHERING_METHOD_OBSERVATION(new Long(29), new GatheringObservationMethod(), true,"sle_gathering_method_observation", GatheringObservationMethod.class.getName()),
+    SPECIMEN_CATEGORY(new Long(36), new SpecimenCategory(), false,"sle_specimen_category", SpecimenCategory.class.getName()),
+    SPECIMEN_TYPE(new Long(37), new SpecimenType(), true,"sle_specimen_type",SpecimenType.class.getName()),
+    ORIGIN(new Long(42), new Origin(), true,"sle_origin",Origin.class.getName()),
+    PRESERVATION_MEDIUM(new Long(44), new PreservationMedium(), true,"sle_preservation_medium",PreservationMedium.class.getName()),
+    STORAGE_TYPE(new Long(40), new StorageType(), true,"sle_storage_type",StorageType.class.getName()),
+    LIFE_STAGE(new Long(43), new LifeStage(), true,"sle_life_stage",LifeStage.class.getName()),
+    SEX(new Long(41), new Sex(), true,"sle_sex",Sex.class.getName()),
+    SUBSTRATE(new Long(134), new Substrate(), true,"sle_substrate",Substrate.class.getName()),
     /*
     IDENTIFICATION_TYPE(10, new IdentificationType(), true),
      */
-    LIFE_FORM(new Long(11),new LifeForm(), true,"sle_life_form",LifeForm.class.getName()),
-    EXTRACTION_TYPE(new Long(13), new ExtractionType(), true,"sle_extraction_type",ExtractionType.class.getName()),
+    LIFE_FORM(new Long(38),new LifeForm(), true,"sle_life_form",LifeForm.class.getName()),
+    EXTRACTION_TYPE(new Long(152), new ExtractionType(), true,"sle_extraction_type",ExtractionType.class.getName()),
     
-    COMPONENT_PART(new Long(14), new ComponentPart(), true, "sle_component_part", ComponentPart.class.getName()),
+    COMPONENT_PART(new Long(54), new ComponentPart(), true, "sle_component_part", ComponentPart.class.getName()),
     /*
     PREPARATION_METHOD(17, new PreparationMethod(), true),
     SAMPLING_TYPE(18, new SamplingType(), true),
      */
-    TAXON_AUTHOR_CONNECTOR(new Long(19), new TaxonAuthorConnector(), true, "sle_taxon_author_connector", TaxonAuthorConnector.class.getName()),
+    TAXON_AUTHOR_CONNECTOR(new Long(132), new TaxonAuthorConnector(), true, "sle_taxon_author_connector", TaxonAuthorConnector.class.getName()),
     /*
     TYPE_SPECIMEN_TYPE(20, new TypeSpecimenType(), true),
     REFERENCE_TYPE(21, new ReferenceType(), true),
     SITE_CALCULATION_METHOD(22, new SiteCalculationMethod(), true),*/
-    EXPOSITION(new Long(23), new Exposition(), true,"sle_exposition",Exposition.class.getName()),
+    EXPOSITION(new Long(154), new Exposition(), true,"sle_exposition",Exposition.class.getName()),
     /*INTERACTION_TYPE(24, new InteractionType(), true),
     GEOGRAPHIC_CATALOGUE(25, new GeographicCatalogue(), true)
      */
-    MATERIAL_TYPE(new Long(27), new MaterialType(), true,"sle_material_type",MaterialType.class.getName()),
-    CROP_SYSTEM(new Long(28), new CropSystem(), true,"sle_crop_system",CropSystem.class.getName()),
-    CROP_TYPE(new Long(29), new CropType(), true,"sle_crop_type",CropType.class.getName()),
-    CULTIVATION_PRACTICE(new Long(30), new CultivationPractice(), true,"sle_cultivation_practice",CultivationPractice.class.getName()),
-    GATHERING_SOURCE(new Long(31), new GatheringSource(), true,"sle_gathering_source",GatheringSource.class.getName()),
-    SAMPLE_STATUS(new Long(32), new SampleStatus(), true,"sle_sample_status",SampleStatus.class.getName()),
-    SOIL_COLOR(new Long(33), new SoilColor(), true,"sle_soil_color",SoilColor.class.getName()),
-    SOIL_TEXTURE(new Long(34), new SoilTexture(), true,"sle_soil_texture",SoilTexture.class.getName()),
+    MATERIAL_TYPE(new Long(155), new MaterialType(), true,"sle_material_type",MaterialType.class.getName()),
+    CROP_SYSTEM(new Long(156), new CropSystem(), true,"sle_crop_system",CropSystem.class.getName()),
+    CROP_TYPE(new Long(157), new CropType(), true,"sle_crop_type",CropType.class.getName()),
+    CULTIVATION_PRACTICE(new Long(158), new CultivationPractice(), true,"sle_cultivation_practice",CultivationPractice.class.getName()),
+    GATHERING_SOURCE(new Long(159), new GatheringSource(), true,"sle_gathering_source",GatheringSource.class.getName()),
+    SAMPLE_STATUS(new Long(160), new SampleStatus(), true,"sle_sample_status",SampleStatus.class.getName()),
+    SOIL_COLOR(new Long(161), new SoilColor(), true,"sle_soil_color",SoilColor.class.getName()),
+    SOIL_TEXTURE(new Long(162), new SoilTexture(), true,"sle_soil_texture",SoilTexture.class.getName()),
 
-    GERMINATION_METHOD_TYPE(new Long(35), new GerminationMethodType(), true,"sle_germination_method_type",GerminationMethodType.class.getName()),
-    COLLECTION_TYPE(new Long(36), new org.inbio.ara.persistence.germplasm.CollectionType(), true,"sle_collection_type",org.inbio.ara.persistence.germplasm.CollectionType.class.getName()),
-    MOISTURE_METHOD_TYPE(new Long(37), new MoistureMethodType(), true,"sle_moisture_method_type",MoistureMethodType.class.getName()),
-    ACCESSION_MOVEMENT_TYPE(new Long(38), new AccessionMovementType(), true,"sle_accession_movement_type",MoistureMethodType.class.getName()),
+    GERMINATION_METHOD_TYPE(new Long(163), new GerminationMethodType(), true,"sle_germination_method_type",GerminationMethodType.class.getName()),
+    COLLECTION_TYPE(new Long(164), new org.inbio.ara.persistence.germplasm.CollectionType(), true,"sle_collection_type",org.inbio.ara.persistence.germplasm.CollectionType.class.getName()),
+    MOISTURE_METHOD_TYPE(new Long(165), new MoistureMethodType(), true,"sle_moisture_method_type",MoistureMethodType.class.getName()),
+    ACCESSION_MOVEMENT_TYPE(new Long(166), new AccessionMovementType(), true,"sle_accession_movement_type",MoistureMethodType.class.getName()),
 
-    CONDITION(new Long(39), new Condition(), true,"sle_condition",Condition.class.getName()),
-    SEMEN_GATHERING_METHOD(new Long(40), new SemenGatheringMethod(), true,"sle_semen_gathering_method",SemenGatheringMethod.class.getName()),
-    SOLVENT(new Long(41), new Solvent(), true,"sle_solvent",Solvent.class.getName()),
+    CONDITION(new Long(167), new Condition(), true,"sle_condition",Condition.class.getName()),
+    SEMEN_GATHERING_METHOD(new Long(168), new SemenGatheringMethod(), true,"sle_semen_gathering_method",SemenGatheringMethod.class.getName()),
+    SOLVENT(new Long(169), new Solvent(), true,"sle_solvent",Solvent.class.getName()),
     
     /* MODULO DE TRANSACCIONES */
-    TRANSACTION_TYPE(new Long(42),new TransactionType(),true,"sle_transaction_type",TransactionType.class.getName()),
-    TRANSACTED_SPECIMEN_STATUS(new Long(43),new TransactedSpecimenStatus(),true,"sle_transacted_specimen_status",TransactedSpecimenStatus.class.getName()),
+    TRANSACTION_TYPE(new Long(170),new TransactionType(),true,"sle_transaction_type",TransactionType.class.getName()),
+    TRANSACTED_SPECIMEN_STATUS(new Long(171),new TransactedSpecimenStatus(),true,"sle_transacted_specimen_status",TransactedSpecimenStatus.class.getName()),
     /* FIN MODULO DE TRANSACCIONES */
 
     /*Agregados al modulo de semen*/
-    SEMEN_CONSISTENCY(new Long(44),new SemenConsistency(),true,"sle_semen_consistency",SemenConsistency.class.getName()),
+    SEMEN_CONSISTENCY(new Long(172),new SemenConsistency(),true,"sle_semen_consistency",SemenConsistency.class.getName()),
     /*fin de los agregados*/
     ;
 
