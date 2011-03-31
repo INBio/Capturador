3140ad42dc36b712aaba1a0bd88e5ded43d9e1a3
gsulca <gsulca@3aa2e761-397d-4522-80f4-092673de1051>
2011-03-31 14:38:24 +0000

    Actualizacion para ingresar listas de seleccion que estaban en ara, pero no en atta
    git-svn-id: svn://pulsatrix.inbio.ac.cr/atta2/trunk@1605 3aa2e761-397d-4522-80f4-092673de1051

--- atta-ejb/Setup/PostgreSQL scripts/atta_update.sql
+++ atta-ejb/Setup/PostgreSQL scripts/atta_update.sql
@@ -19,6 +19,7 @@
 --2011.03.23 gsulca
 
 --Set start profile_seq
+-- usar: select setval('atta.identification_history_seq',cast(max(identification_history_id) as bigint)+1) from atta.identification_history;
 ALTER SEQUENCE ATTA.PROFILE_SEQ RESTART WITH 22;
 
 -- Adding profiles
@@ -44,6 +45,75 @@ INSERT INTO ATTA.TAXON (taxonomical_range_id, current_name, current_name_timesta
 
 UPDATE ATTA.TAXON SET ancestor_id = 74762 WHERE taxonomical_range_id = 23;
 
+--2011.03.30 gsulca
+
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (154,'Exposition',0,'atta','2011-03-30','atta','2011-03-30','exposition','exposition_id');
+
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (155,'Tipo de Material',0,'atta','2010-01-21','atta','2010-01-21','material_type','material_type_id');
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (156,'Sistema de Cultivo',0,'atta','2010-01-21','atta','2010-01-21','crop_system','crop_system_id');
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (157,'Tipo de Cultivo',0,'atta','2010-01-21','atta','2010-01-21','crop_type','crop_type_id');
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (158,'Practica de Cultivo',0,'atta','2010-01-21','atta','2010-01-21','cultivation_practice','cultivation_practice_id');
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (159,'Fuente de Recoleccion',0,'atta','2010-01-21','atta','2010-01-21','gathering_source','gathering_source_id');
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (160,'Estado de la muestra',0,'atta','2010-01-21','atta','2010-01-21','sample_status','sample_status_id');
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (161,'Color de Suelo',0,'atta','2010-01-21','atta','2010-01-21','soil_color','soil_color_id');
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (162,'Textura del Suelo',0,'atta','2010-01-21','atta','2010-01-21','soil_texture','soil_texture_id');
+
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (163,'Método de Germinación',0,'atta','2010-03-03','atta','2010-03-03','germination_method_type','germination_method_type_id');
+
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (164,'Tipo de Colección',0,'atta','2010-03-03','atta','2010-03-03','collection_type','collection_type_id');
+
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (165,'Método de Humedad',0,'atta','2010-03-03','atta','2010-03-03','moisture_method_type','moisture_method_type_id');
+
+INSERT INTO atta.list_table (list_table_id,description,obj_version,created_by,creation_date,last_modification_by,last_modification_date,name,key_field_name)
+values (166,'Movimiento de Accesión',0,'atta','2010-03-03','atta','2010-03-03','accession_movement_type','accession_movement_type_id');
+
+INSERT INTO atta.list_table(
+            list_table_id, description, obj_version, created_by, creation_date,
+            last_modification_by, last_modification_date, "name", key_field_name)
+    VALUES (167, '', 0, 'Ara', '2010-04-13',
+            'Ara', '2010-04-13', 'condition', 'condition_id');
+
+INSERT INTO atta.list_table(
+            list_table_id, description, obj_version, created_by, creation_date,
+            last_modification_by, last_modification_date, "name", key_field_name)
+    VALUES (168, '', 0, 'Ara', '2010-04-13',
+            'Ara', '2010-04-13', 'semen_gathering_method', 'semen_gathering_method_id');
+
+INSERT INTO atta.list_table(
+            list_table_id, description, obj_version, created_by, creation_date,
+            last_modification_by, last_modification_date, "name", key_field_name)
+    VALUES (169, '', 0, 'Ara', '2010-04-13',
+            'Ara', '2010-04-13', 'solvent', 'solvent_id');
+
+INSERT INTO atta.list_table(
+            list_table_id, description, obj_version, created_by, creation_date,
+            last_modification_by, last_modification_date, "name", key_field_name)
+    VALUES (170, 'Tipo de Transacción', 0, 'atta', '2010-03-01',
+            'atta', '2010-03-01', 'transaction_type', 'transaction_type_id');
+
+INSERT INTO atta.list_table(
+            list_table_id, description, obj_version, created_by, creation_date,
+            last_modification_by, last_modification_date, "name", key_field_name)
+    VALUES (171, 'Estado de Espécimen Transado', 0, 'atta', '2010-03-01',
+            'atta', '2010-03-01', 'transacted_specimen_status', 'transacted_specimen_status_id');
+
+INSERT INTO atta.list_table(
+            list_table_id, description, obj_version, created_by, creation_date,
+            last_modification_by, last_modification_date, "name", key_field_name)
+    VALUES (172, '', 0, 'Ara', '2010-8-12',
+            'Ara', '2010-8-12', 'semen_consistency', 'semen_consistency_id');
 
 
 
