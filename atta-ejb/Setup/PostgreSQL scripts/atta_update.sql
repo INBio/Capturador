c5a67149f6f175f09b5f009e90f622108e1146f7
gsulca <gsulca@3aa2e761-397d-4522-80f4-092673de1051>
2011-04-13 14:54:58 +0000

    Actualizacion de los sequence
    git-svn-id: svn://pulsatrix.inbio.ac.cr/atta2/trunk@1626 3aa2e761-397d-4522-80f4-092673de1051

--- atta-ejb/Setup/PostgreSQL scripts/atta_update.sql
+++ atta-ejb/Setup/PostgreSQL scripts/atta_update.sql
@@ -120,3 +120,187 @@ INSERT INTO atta.list_table(
 --Set start gathering_observation_seq
 select setval('atta.gathering_observation_seq',cast(max(gathering_observation_id) as bigint)+1) from atta.gathering_observation;
 
+-- 2011.04.12 gsulca
+
+--Set start accession_movement_type_seq
+select setval('atta.accession_movement_type_seq',cast(max(accession_movement_type_id) as bigint)+1) from atta.accession_movement_type;
+
+--Set start accession_seq
+select setval('atta.accession_seq',cast(max(accession_id) as bigint)+1) from atta.accession;
+
+--Set start audience_seq
+select setval('atta.audience_seq',cast(max(audience_id) as bigint)+1) from atta.audience;
+
+--Set start breed_seq
+select setval('atta.breed_seq',cast(max(breed_id) as bigint)+1) from atta.breed;
+
+--Set start collection_seq
+select setval('atta.collection_seq',cast(max(collection_id) as bigint)+1) from atta.collection;
+
+--Set start collection_type_seq
+select setval('atta.collection_type_seq',cast(max(collection_type_id) as bigint)+1) from atta.collection_type;
+
+--Set start component_part_seq
+select setval('atta.component_part_seq',cast(max(component_part_id) as bigint)+1) from atta.component_part;
+
+--Set start condition_seq
+select setval('atta.condition_seq',cast(max(condition_id) as bigint)+1) from atta.condition;
+
+--Set start country_seq
+select setval('atta.country_seq',cast(max(country_id) as bigint)+1) from atta.country;
+
+--Set start crop_system_seq
+select setval('atta.crop_system_seq',cast(max(crop_system_id) as bigint)+1) from atta.crop_system;
+
+--Set start crop_type_seq
+select setval('atta.crop_type_seq',cast(max(crop_type_id) as bigint)+1) from atta.crop_type;
+
+--Set start cultivation_practice_seq
+select setval('atta.cultivation_practice_seq',cast(max(cultivation_practice_id) as bigint)+1) from atta.cultivation_practice;
+
+--Set start dublin_core_description_seq
+select setval('atta.dublin_core_description_seq',cast(max(dublin_core_description_id) as bigint)+1) from atta.dublin_core_description;
+
+--Set start dublin_core_element_seq
+select setval('atta.dublin_core_element_seq',cast(max(dublin_core_element_id) as bigint)+1) from atta.dublin_core_element;
+
+--Set start dublin_core_element_seq
+select setval('atta.dublin_core_element_seq',cast(max(dublin_core_element_id) as bigint)+1) from atta.dublin_core_element;
+
+--Set start exposition_seq
+select setval('atta.exposition_seq',cast(max(exposition_id) as bigint)+1) from atta.exposition;
+
+--Set start extraction_type_seq
+select setval('atta.extraction_type_seq',cast(max(extraction_type_id) as bigint)+1) from atta.extraction_type;
+
+--Set start gathering_observation_detail_seq
+select setval('atta.gathering_observation_detail_seq',cast(max(gathering_observation_detail_id) as bigint)+1) from atta.gathering_observation_detail;
+
+--Set start gathering_observation_method_seq
+select setval('atta.gathering_observation_method_seq',cast(max(gathering_observation_method_id) as bigint)+1) from atta.gathering_observation_method;
+
+--Set start gathering_observation_seq
+select setval('atta.gathering_observation_seq',cast(max(gathering_observation_id) as bigint)+1) from atta.gathering_observation;
+
+--Set start gathering_source_seq
+select setval('atta.gathering_source_seq',cast(max(gathering_source_id) as bigint)+1) from atta.gathering_source;
+
+--Set start germination_method_type_seq
+select setval('atta.germination_method_type_seq',cast(max(germination_method_type_id) as bigint)+1) from atta.germination_method_type;
+
+--Set start identification_history_seq
+select setval('atta.identification_history_seq',cast(max(identification_history_id) as bigint)+1) from atta.identification_history;
+
+--Set start indicator_seq
+select setval('atta.indicator_seq',cast(max(indicator_id) as bigint)+1) from atta."indicator";
+
+--Set start institution_seq
+select setval('atta.institution_seq',cast(max(institution_id) as bigint)+1) from atta.institution;
+
+--Set start life_form_seq
+select setval('atta.life_form_seq',cast(max(life_form_id) as bigint)+1) from atta.life_form;
+
+--Set start life_stage_seq
+select setval('atta.life_stage_seq',cast(max(life_stage_id) as bigint)+1) from atta.life_stage;
+
+--Set start material_type_seq
+select setval('atta.material_type_seq',cast(max(material_type_id) as bigint)+1) from atta.material_type;
+
+--Set start moisture_method_type_seq
+select setval('atta.moisture_method_type_seq',cast(max(moisture_method_type_id) as bigint)+1) from atta.moisture_method_type;
+
+--Set start morphological_description_seq
+select setval('atta.morphological_description_seq',cast(max(morphological_description_id) as bigint)+1) from atta.morphological_description;
+
+--Set start nomenclatural_group_seq
+select setval('atta.nomenclatural_group_seq',cast(max(nomenclatural_group_id) as bigint)+1) from atta.nomenclatural_group;
+
+--Set start origin_seq
+select setval('atta.origin_seq',cast(max(origin_id) as bigint)+1) from atta.origin;
+
+--Set start passport_seq
+select setval('atta.passport_seq',cast(max(passport_id) as bigint)+1) from atta.passport;
+
+--Set start person_seq
+select setval('atta.person_seq',cast(max(person_id) as bigint)+1) from atta.person;
+
+--Set start preservation_medium_seq
+select setval('atta.preservation_medium_seq',cast(max(preservation_medium_id) as bigint)+1) from atta.preservation_medium;
+
+--Set start profile_seq
+select setval('atta.profile_seq',cast(max(profile_id) as bigint)+1) from atta.profile;
+
+--Set start province_seq
+select setval('atta.province_seq',cast(max(province_id) as bigint)+1) from atta.province;
+
+--Set start sample_status_seq
+select setval('atta.sample_status_seq',cast(max(sample_status_id) as bigint)+1) from atta.sample_status;
+
+--Set start semen_gathering_method_seq
+select setval('atta.semen_gathering_method_seq',cast(max(semen_gathering_method_id) as bigint)+1) from atta.semen_gathering_method;
+
+--Set start semen_gathering_seq
+select setval('atta.semen_gathering_seq',cast(max(semen_gathering_id) as bigint)+1) from atta.semen_gathering;
+
+--Set start semental_seq
+select setval('atta.semental_seq',cast(max(semental_id) as bigint)+1) from atta.semental;
+
+--Set start sex_seq
+select setval('atta.sex_seq',cast(max(sex_id) as bigint)+1) from atta.sex;
+
+--Set start site_coordinate_seq
+select setval('atta.site_coordinate_seq',cast(max(site_coordinate_id) as bigint)+1) from atta.site_coordinate;
+
+--Set start site_seq
+select setval('atta.site_seq',cast(max(site_id) as bigint)+1) from atta.site;
+
+--Set start soil_color_seq
+select setval('atta.soil_color_seq',cast(max(soil_color_id) as bigint)+1) from atta.soil_color;
+
+--Set start soil_texture_seq
+select setval('atta.soil_texture_seq',cast(max(soil_texture_id) as bigint)+1) from atta.soil_texture;
+
+--Set start solvent_seq
+select setval('atta.solvent_seq',cast(max(solvent_id) as bigint)+1) from atta.solvent;
+
+--Set start specimen_category_seq
+select setval('atta.specimen_category_seq',cast(max(specimen_category_id) as bigint)+1) from atta.specimen_category;
+
+--Set start specimen_seq
+select setval('atta.specimen_seq',cast(max(specimen_id) as bigint)+1) from atta.specimen;
+
+--Set start specimen_type_seq
+select setval('atta.specimen_type_seq',cast(max(specimen_type_id) as bigint)+1) from atta.specimen_type;
+
+--Set start storage_type_seq
+select setval('atta.storage_type_seq',cast(max(storage_type_id) as bigint)+1) from atta.storage_type;
+
+--Set start substrate_seq
+select setval('atta.substrate_seq',cast(max(substrate_id) as bigint)+1) from atta.substrate;
+
+--Set start system_user_seq
+select setval('atta.system_user_seq',cast(max(system_user_id) as bigint)+1) from atta."system_user";
+
+--Set start taxon_author_connector_seq
+select setval('atta.taxon_author_connector_seq',cast(max(taxon_author_connector_id) as bigint)+1) from atta.taxon_author_connector;
+
+--Set start taxon_description_element_seq
+select setval('atta.taxon_description_element_seq',cast(max(taxon_description_element_id) as bigint)+1) from atta.taxon_description_element;
+
+--Set start taxon_description_record_seq
+select setval('atta.taxon_description_record_seq',cast(max(taxon_description_record_id) as bigint)+1) from atta.taxon_description_record;
+
+--Set start taxon_description_stage_seq
+select setval('atta.taxon_description_stage_seq',cast(max(taxon_description_stage_id) as bigint)+1) from atta.taxon_description_stage;
+
+--Set start taxon_seq
+select setval('atta.taxon_seq',cast(max(taxon_id) as bigint)+1) from atta.taxon;
+
+--Set start transacted_specimen_status_seq
+select setval('atta.transacted_specimen_status_seq',cast(max(transacted_specimen_status_id) as bigint)+1) from atta.transacted_specimen_status;
+
+--Set start transaction_seq
+select setval('atta.transaction_seq',cast(max(transaction_id) as bigint)+1) from atta."transaction";
+
+--Set start transaction_type_seq
+select setval('atta.transaction_type_seq',cast(max(transaction_type_id) as bigint)+1) from atta.transaction_type;
