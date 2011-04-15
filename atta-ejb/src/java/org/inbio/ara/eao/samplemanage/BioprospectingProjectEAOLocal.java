8a1bfa9ba441b6ec1b58155ca19d512ce1768dad
dasolano <dasolano@3aa2e761-397d-4522-80f4-092673de1051>
2011-04-15 15:51:32 +0000

    Estructura para el modulo de muestras
    git-svn-id: svn://pulsatrix.inbio.ac.cr/atta2/trunk@1634 3aa2e761-397d-4522-80f4-092673de1051

---
+++ atta-ejb/src/java/org/inbio/ara/eao/samplemanage/BioprospectingProjectEAOLocal.java
@@ -0,0 +1,32 @@
+/* Ara - capture species and specimen data
+ *
+ * Copyright (C) 2009  INBio (Instituto Nacional de Biodiversidad)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.inbio.ara.eao.samplemanage;
+
+import javax.ejb.Local;
+import org.inbio.ara.eao.BaseLocalEAO;
+import org.inbio.ara.persistence.samplemanage.BioprospectingProject;
+
+/**
+ *
+ * @author dasolano
+ */
+@Local
+public interface BioprospectingProjectEAOLocal extends BaseLocalEAO<BioprospectingProject ,Long> {
+    
+}
